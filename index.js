784fac80352501f13f99c4304d7e730ae0d8a577
Chow Minh Khang <khangchow@gmail.com>
2022-02-13 16:55:55 +0700

    init project

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+  console.log('a user connected ', socket.id);
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+
+  socket.on('newUser', (username) => {
+    console.log(username);
+    io.emit('newUser', {
+      id: socket.id,
+      username: username
+    });
+  });
+
+  socket.on('chatMessage', (msg) => {
+    console.log(msg);
+    io.emit('chatMessage', msg);
+  });
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
