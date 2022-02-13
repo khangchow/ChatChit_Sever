092d18fb525f997003a8c6d416d0ec006d72555a
Chow Minh Khang <khangchow@gmail.com>
2022-02-13 21:17:50 +0700

    save user data to emit disconnect message for others

--- index.js
+++ index.js
@@ -5,24 +5,35 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+var users = [];
+
 io.on('connection', (socket) => {
   console.log('a user connected ', socket.id);
 
   socket.on('disconnect', () => {
     console.log('user disconnected');
+    socket.broadcast.emit('userState', {
+        username: users.find((user) => user.id === socket.id).username,
+        state: 'left'
+    });
   });
 
   socket.on('newUser', (username) => {
     console.log(username);
+
+    users.push({
+      id: socket.id,
+      username: username
+    });
 
     socket.emit('self', {
       id: socket.id,
       username: username
     });
 
-    socket.broadcast.emit('newUser', {
-      id: socket.id,
-      username: username
+    socket.broadcast.emit('userState', {
+      username: username,
+      state: 'joined'
     });
   });
 
